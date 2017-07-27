537e894f01fec57af662e708394bfaab3e4f79f9
Linlin <hanlinlin@live.com>
2017-07-26 20:34:47 -0400

    Created GiantPlanet class

---
+++ COMP123_S2017_Assignment4/GiantPlanet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Name: Linlin Han
+ * Date: July 26, 2017
+ * Description: This is Abstract Planet class
+ * Version: 0.1 - Created GiantPlanet class
+ */
+
+namespace COMP123_S2017_Assignment4
+{
+    /// <summary>
+    /// This is GiantPlanet class.
+    /// </summary>
+    public class GiantPlanet : Planet
+    {
+        // PRIVATE INSTANCE VARIABLES (FIELDS)
+        private string _type;
+
+        // PUBLIC PROPERTIES
+
+
+        // CONSTRUCTORS
+        public GiantPlanet(string name, double diameter, double mass, string type)
+            : base(name, diameter, mass)
+        {
+            this._type = type;
+        }
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+    }
+}
